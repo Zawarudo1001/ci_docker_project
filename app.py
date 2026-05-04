5e3497e97984682422850ce80629756557ef3e5d
Zawarudo1001 <timohinvaceslav4@gmail.com>
2026-05-04 04:23:26 +0300

    added some new functions and unit-tests

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    print("Flask App runs in normal mode")
     return 'Hello, Docker CI/CD! from Timokhin V.M.'
+
+@app.route("/api/sum/<int:a>/<int:b>")
+def get_sum(a, b):
+    return jsonify({"result": a + b})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,17 @@ def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
     assert response.data == b'Hello, Docker CI/CD! from Timokhin V.M.'
+
+
+def test_api_sum(client):
+    response = client.get("/api/sum/5/7")
+    
+    assert response.status_code == 200
+    # Получаем данные сразу в формате JSON
+    json_data = response.get_json()
+    assert json_data["result"] == 12, "Калькулятор посчитал неверно"
+
+def test_404_page(client):
+    response = client.get("/non-existent")
+    
+    assert response.status_code == 404
